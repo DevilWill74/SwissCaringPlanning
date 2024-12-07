b15fb511a4e854e1ca364ee4255603e60c78a646
DevilWill74 <alexis.fauqueur@outlook.fr>
2024-12-07 14:20:56 +0100

    test

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -40,17 +40,15 @@ export default function Login() {
   };
 
   return (
-<div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-  <div className="sm:mx-auto sm:w-full sm:max-w-md">
-    <div className="flex justify-center">
-      {/* Augmentation de la taille de l'image */}
-      <img src={logoSwiss} alt="Logo Swiss" className="h-24 w-24" />
-    </div>
-    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-      Planning Infirmier
-    </h2>
-  </div>
-</div>
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="flex justify-center">
+          <img src={logoSwiss} alt="Logo Swiss" className="h-24 w-36" />
+        </div>
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Planning Infirmier
+        </h2>
+      </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
